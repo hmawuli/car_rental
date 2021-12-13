54faf98516d091ec2f0b1b50d8f63de5e0a44225
Hormeku Mawuli <hormekumawuli93@gmail.com>
2021-12-13 05:58:02 +0000

    connecting databse

--- app/Http/Controllers/CarRentalController.php
+++ app/Http/Controllers/CarRentalController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\admin;
 use Illuminate\Http\Request;
 
 class CarRentalController extends Controller
 {
-    //
+    public function index(){
+        $admin = admin::all();
+    }
+
+    
 }
