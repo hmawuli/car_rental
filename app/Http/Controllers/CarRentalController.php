85fbd9b87cdd18bb525ad5f7cdde1a6d8a40f9ac
Hormeku Mawuli <hormekumawuli93@gmail.com>
2021-12-13 09:46:19 +0000

    functions

--- app/Http/Controllers/CarRentalController.php
+++ app/Http/Controllers/CarRentalController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\admin;
+use App\Models\car_image;
 use Illuminate\Http\Request;
 
 class CarRentalController extends Controller
@@ -11,5 +12,28 @@ class CarRentalController extends Controller
         $admin = admin::all();
     }
 
-    
+   public function create(){
+
+   }
+
+   public function store(Request $request ){
+
+   }
+
+   public function show($id){
+
+   }
+
+   public function edit($id){
+
+   }
+
+   public function update(Request $request, $id){
+
+   }
+
+   public function destroy($id){
+       
+   }
+
 }
