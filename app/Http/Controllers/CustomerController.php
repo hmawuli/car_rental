ab4f228c8ad667f6b7ce777bf5d248cbf45efc63
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-18 02:14:14 +0000

    customer function

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\customer;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CustomerController extends Controller
 {
-    //
+    public function show(customer $customer)
+    {
+        return Inertia::render('customer/show',[
+            'customer' => $customer->only(
+                'id',
+                'customer_name',
+                'address',
+                'contact',
+                'profile-image',
+                'email_account',
+                'username',
+                'password',
+                'account-status',
+
+
+
+            ),
+
+
+        ]);
+
+
+    }
 }
