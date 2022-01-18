d7b86985199a6179bb7d45595724194df49b7b14
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-18 01:46:32 +0000

    pages controller

---
+++ app/Http/Controllers/CustomerCredentialController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerCredentialController extends Controller
+{
+    //
+}
---
+++ app/Models/car_rental.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class car_rental extends Model
+{
+    use HasFactory;
+}
