ed38b794ce8157c860af16a14a4da82775e0b4bd
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-19 11:27:09 +0000

    car owner

--- app/Http/Controllers/CarOwnerController.php
+++ app/Http/Controllers/CarOwnerController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\car_owner;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CarOwnerController extends Controller
 {
-    //
+    public function car_owner(car_owner $car_owner){
+        return Inertia::render('car_owner',[
+            'car_owner' => $car_owner -> only(
+                'id',
+                'owner_name',
+                'address',
+                'contact',
+                'profile_image',
+                'email_account',
+                'username',
+                'password',
+                'admin_id',
+                'account_status',
+            ),
+        ]);
+    }
 }
