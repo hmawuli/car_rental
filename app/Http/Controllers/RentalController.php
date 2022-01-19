eb8a52a33b36005ca8c9383be6fd22e2faea7c1e
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-19 10:08:43 +0000

    rental contrller pages

--- app/Http/Controllers/RentalController.php
+++ app/Http/Controllers/RentalController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\rental;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class RentalController extends Controller
 {
-    //
+    public function rental(rental $rental){
+        return Inertia::render('rental',[
+            'rental'=> $rental-> only(
+                'id',
+                'rental_date',
+                'rental_time',
+                'return_time',
+                'owner-id',
+                'car_id',
+                'customer_id',
+                'rental_status',
+            ),
+        ]);
+    }
 }
