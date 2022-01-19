c2ac805e02ae85af04538988b247e03c1802b15c
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-19 10:01:40 +0000

    payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -1,13 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\payment;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PaymentController extends Controller
 {
-    public function payment(){
-        return Inertia::render();
+    public function payment(payment $payment){
+        return Inertia::render('payment',[
+            'payment' => $payment-> only(
+                'id',
+                'rental_id',
+                'payment_amount',
+                'add_charges',
+                'payment_date',
+                'admin_id',
+
+
+            ),
+        ]);
     }
 }
