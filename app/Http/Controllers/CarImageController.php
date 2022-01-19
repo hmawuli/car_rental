fbceaa9627621e1285c3ccc604c88952dca4affb
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-19 11:37:39 +0000

    car image

--- app/Http/Controllers/CarImageController.php
+++ app/Http/Controllers/CarImageController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\car_image;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CarImageController extends Controller
 {
-    //
+    public function car_image(car_image $car_image){
+        return Inertia::render('car_image',[
+            'car_image'=> $car_image-> only(
+                'id',
+                'image_description',
+                'car_id',
+            ),
+        ]);
+    }
 }
