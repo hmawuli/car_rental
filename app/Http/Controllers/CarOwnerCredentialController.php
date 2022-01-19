be6ab9ad8bf9de63efbcab7d606bbb7b52b9d0a2
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-19 11:13:55 +0000

    car owner

--- app/Http/Controllers/CarOwnerCredentialController.php
+++ app/Http/Controllers/CarOwnerCredentialController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\car_owner_credential;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CarOwnerCredentialController extends Controller
 {
-    //
+    public function car_owner_credential(car_owner_credential $car_owner_credential){
+        return Inertia::render('car_owner_credential',[
+            'car_owner_credential' => $car_owner_credential-> only(
+                'id',
+                'credential_name',
+                'file_upload',
+                'owner_id',
+            ),
+        ]);
+    }
 }
