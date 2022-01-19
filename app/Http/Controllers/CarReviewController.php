513adf1816c95955e0d3f488972a4ff8122659af
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-19 10:47:39 +0000

    car review controllers

--- app/Http/Controllers/CarReviewController.php
+++ app/Http/Controllers/CarReviewController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\car_review;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CarReviewController extends Controller
 {
-    //
+    public function car_review( car_review $car_review){
+        return Inertia::render('car_review',[
+            'car_review' => $car_review -> only(
+                'id',
+                'review',
+                'review_score',
+                'date',
+                'customer_id',
+                'car_id',
+            ),
+        ]);
+
+    }
 }
