e4aa8dac1855d3519cf4946b99ec005a666ccb1c
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-19 09:35:20 +0000

    pages contrller

--- app/Http/Controllers/CustomerCredentialController.php
+++ app/Http/Controllers/CustomerCredentialController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\customer;
+use App\Models\custmoer_credential;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CustomerCredentialController extends Controller
 {
-    //
+    public function custmoer_credential(custmoer_credential $custmoer_credential){
+        return Inertia::render('CustomerCredential',[
+           'customer_credential' => $custmoer_credential-> only(
+               'id',
+               'credential_name',
+               'file_Upload',
+               'customer_id',
+
+           ),
+        ]);
+
+    }
 }
