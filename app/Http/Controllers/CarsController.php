0accb3c7f645e593ec1354284c390df5e7cb481b
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-19 10:24:18 +0000

    cars controller pages

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -1,10 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\cars;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CarsController extends Controller
 {
-    //
+    public function cars(cars $cars){
+        return Inertia::render('cars',[
+            'cars'=> $cars-> only(
+                'id',
+                'car_name',
+                'description',
+                'model_year',
+                'car_brand',
+                'color',
+                'capacity',
+                'plate_number',
+                'rate',
+                'owner_id',
+                'status',
+
+            ),
+        ]);
+
+
+    }
 }
