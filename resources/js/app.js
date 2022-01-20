7c33e251da3b8fd607f800d6eb8819a92a1004f5
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-20 09:02:11 +0000

    additions to the app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,28 @@
-
-
 require('./bootstrap');
 
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, App, props, plugin }) {
+    Vue.use(plugin)
+
+    new Vue({
+      render: h => h(App, props),
+    }).$mount(el)
+  },
+})
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, App, props, plugin }) {
+    Vue.use(plugin)
+
+    new Vue({
+      render: h => h(App, props),
+    }).$mount(el)
+  },
+})
