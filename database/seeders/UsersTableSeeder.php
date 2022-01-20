fc0795d36dc712f6beef7a00417bf46a241b92f8
hormeku mawuli <hormekumawuli93@gmail.com>
2022-01-20 08:23:48 +0000

    changes made to the database seeders

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use App\Http\Database\Seeder\user;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        user::create([
+
+
+        ]);
+    }
+}
