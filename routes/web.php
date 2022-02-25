42bc88964a2ba08b7269410f3aa61dcf6473ec5e
hormeku mawuli <hormekumawuli93@gmail.com>
2022-02-25 11:35:46 +0000

    corrections made on web.php folder

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::get('dashboard', [DashboardController::class, 'index']);
 
 //Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
